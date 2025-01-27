fb4435aeae04ea88bc0b54c6df04ab6f56f71ac3
anvitgupta01 <anvitiitr@gmail.com>
2025-01-27 10:40:27 +0530

    Update the code of Jarvis for latest pip version
    1. Involve change in qrcode generator
    2. Modifying requirements.txt accordingly.

--- qrcodeGen.py
+++ qrcodeGen.py
@@ -1,12 +1,9 @@
-import qrcode
-data = input("Enter data or site to encode in the QR ...")
-image = input("Input the file name along with extension for QR to be saved into ")
-qr = qrcode.QRCode(version = 1,
-                error_correction = qrcode.constants.ERROR_CORRECT_H,   
-				box_size = 10,
-				border = 5)
-qr.add_data(data)
-qr.make(fit = True)
-img = qr.make_image(fill_color = 'black',
-					back_color = 'white')
-img.save(f'{image}')
+import pyqrcode
+s = input("Enter the data or text to be encoded in QR code...")
+f = input("Input the name of the file in which QR Code have to be saved along with extension...")
+
+url = pyqrcode.create(s) 
+if (f.endswith("svg")) :
+    url.svg(f, scale = 8) 
+else :
+    url.png(f, scale = 8) 
